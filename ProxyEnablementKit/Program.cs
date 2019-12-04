bc8ab230b41d4a175db97f5da267881dff63864d
Risa Mizushina <risa.mizushina@gmail.com>
2019-12-04 20:07:47 +0900

    作成中（バックアップも兼ねて、いったんコミットしますが、まだ全然、完成していません）

---
+++ ProxyEnablementKit/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+
+namespace ProxyEnablementKit
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // 前提条件として、 nuget.exeの、存在を確認します
+            if(!CheckNugetExe())
+            {
+                Console.WriteLine("Nuget.exeが、見つかりませんでした。");
+                return;
+            }
+
+            
+        }
+
+        /// <summary>
+        /// Nuget.exeの、存在確認
+        /// </summary>
+        /// <returns></returns>
+        static bool CheckNugetExe()
+        {
+            return File.Exists(Path.Combine(Util.AppDirectory, "nuget.exe"));
+        }
+    }
+}
---
+++ ProxyEnablementKit/Util.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProxyEnablementKit
+{
+    internal class Util
+    {
+        /// <summary>
+        /// アプリケーションのディレクトリ
+        /// </summary>
+        internal static string AppDirectory
+        {
+            get
+            {
+                return System.IO.Path.GetDirectoryName(System.Diagnostics.Process.GetCurrentProcess().MainModule.FileName);
+            }
+        }
+    }
+}
